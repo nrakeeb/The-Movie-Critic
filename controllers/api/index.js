3e8cdb51a335358c043e4663241a308f8315aa15
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-01-30 18:47:10 +0000

    chore: changed routes to include movie-routes
    Author: NR

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const MovieRoutes = require('./movie-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes)
+router.use('/movies', MovieRoutes)
 
 module.exports = router;
