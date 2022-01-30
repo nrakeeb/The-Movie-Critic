a38c583f540679b284ff161a87e8d9ab6ce7a51e
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-01-30 18:54:47 +0000

    chore: added foreignkey for movie
    Author : NR

--- models/index.js
+++ models/index.js
@@ -3,19 +3,36 @@ const Post = require('./Post');
 const Movie = require('./Movie')
 
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+})
+
+User.hasMany(Movie, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
 })
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+
 })
 
 Movie.hasMany(Post, {
-    foreignKey: "movie_id"
+    foreignKey: "movieId",
+    onDelete: 'CASCADE'
+
 })
 
 Post.belongsTo(Movie, {
-    foreignKey: "movie_id"
+    foreignKey: "movieId", 
+    onDelete: 'CASCADE'
+
+})
+
+Movie.belongsTo(User,{
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
 })
 
 
