d2802f1c474835b340fb859bc1516e14ec78d1e2
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2022-01-31 16:24:22 +0000

    chore: created a post reponse by targeting the elements from the movie-search.js file
    Author: NR

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,25 +1,38 @@
 async function newFormHandler(event) {
-    event.preventDefault();
+    event.preventDefault(); 
 
     const ranking = document.querySelector('input[name="post-title"]').value;
     const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
+    const movie_title_element = document.getElementsByClassName("movie-title")[0]; // finds the movie element from the innerHTML in searchMovies
+    const moviePoster = document.getElementById("movie-id").src; // finding the element of movie-id 
 
-    const response = await fetch(`/api/posts`, {
+
+    const movieReponse = await postToAPI(
+        '/api/movies',
+        JSON.stringify({"imdb_id":movie_title_element.id,"title":movie_title_element.textContent,"image_url":moviePoster}));
+        // saves movie into the database
+    let movie_id = movieReponse.movie_id; //brings back the object
+        console.log(movieReponse)
+    postToAPI('/api/posts',JSON.stringify({
+        ranking,
+        post_content,
+        movie_id
+    })).then( 
+        response => {
+             document.location.replace('/dashboard')
+        })
+
+}
+
+async function postToAPI(url,data){
+    const response = await fetch(url, {
         method: 'POST',
-        body: JSON.stringify({
-            ranking,
-            post_content
-        }),
+        body: data,
         headers: {
             'Content-Type': 'application/json'
         }
     });
-
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+    return response.json(); 
 }
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
